1524cbf97b08dc70ee77400cce31ada84bdedf59
Mohammed Sadiq K <md.sdq.k@gmail.com>
2022-10-09 23:14:47 +0530

    added middleware logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,17 @@
-import { NextFetchEvent, NextRequest } from "next/server";
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
-export function middleware(req: NextRequest, ev: NextFetchEvent) {
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
 	if (req.nextUrl.pathname.startsWith("/api/get-url/")) {
 		console.log("\n returning early");
 		return;
 	}
+
+	const slug = req.nextUrl.pathname.split("/").pop();
+	//console.log("fetching from", `${req.nextUrl}/api/get-url/${slug}`);
+	const data = await (await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`)).json();
+
+	if (data?.url) {
+		return NextResponse.redirect(data.url);
+	}
+
 }
